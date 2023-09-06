40a6047c5d51f5f6607f164024a72de37f66d860
erhimu ebrue <erhimuebru87@gmail.com>
2023-09-06 02:53:13 +0100

    new

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const PORT = 6000
+  await app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}
+    Database connected successfully`)
+  });
 }
 bootstrap();
+
+
+
